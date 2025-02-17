35fc90a7672965a4bd8534c6c9076ce0fc0f8de7
KruthikaNadiger <kruthikanadiger@gmail.com>
2025-02-17 12:12:05 +0530

    Initial commit

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderProcessingApp.Models;
+
+namespace OrderProcessingApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        [HttpGet]
+        public IActionResult CreateOrder()
+        {
+            return View("CreateOrder");
+        }
+
+        [HttpPost]
+        public IActionResult Submit(Order order)
+        {
+            if (order.OrderAmount <= 0 || (order.CustomerType != "New" && order.CustomerType != "Loyal"))
+            {
+                ModelState.AddModelError(string.Empty, "Invalid input.");
+                return View("CreateOrder");
+            }
+
+            order.CalculateDiscount();
+            return View("ResultOrder", order);
+        }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,26 @@
+﻿namespace OrderProcessingApp.Models
+{
+    public class Order
+    {
+        public decimal OrderAmount { get; set; }
+        public string CustomerType { get; set; }
+        public decimal Discount { get; set; }
+        public decimal FinalTotal { get; set; }
+
+        public void CalculateDiscount()
+        {
+            if (OrderAmount >= 100 && CustomerType == "Loyal")
+            {
+                Discount = OrderAmount * 0.1m; // 10% discount
+            }
+            else
+            {
+                Discount = 0;
+            }
+
+            FinalTotal = OrderAmount - Discount;
+        }
+
+    }
+
+}
